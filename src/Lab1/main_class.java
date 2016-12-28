9fb909605fb952478ddc3122fb05e0fbb34b7c73
LiKViDaToR696 <wwwww.ru.ua@mail.ru>
2016-12-29 00:25:19 +0200

    Threetd commit

--- src/Lab1/main_class.java
+++ src/Lab1/main_class.java
@@ -7,7 +7,7 @@ public class main_class {
 
     public static void main(String[] args){
 
-        System.out.println("Hello world fn21487");
+        System.out.println("Hello world i destroy you)))");
     }
 
 
