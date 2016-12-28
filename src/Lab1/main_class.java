327cc2eef36bfca81de193e35b034781614b1550
LiKViDaToR696 <wwwww.ru.ua@mail.ru>
2016-12-29 00:13:28 +0200

    first commit

---
+++ src/Lab1/main_class.java
@@ -0,0 +1,15 @@
+package Lab1;
+
+/**
+ * Created by Komp on 20.09.2016.
+ */
+public class main_class {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello world");
+    }
+
+
+
+}
